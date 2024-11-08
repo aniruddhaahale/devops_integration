9647880b2419112aafd84f5bab6fb16b2808cbf0
Aniruddha Ahale <107400159+aniruddhaahale@users.noreply.github.com>
2024-11-08 22:14:09 +0530

    first commit

---
+++ src/main/java/com/devops/devops/Controller/UserController.java
@@ -0,0 +1,12 @@
+package com.devops.devops.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @GetMapping("/")
+    public String getmessageString(){
+        return "Hello, World!";
+    }
+}
